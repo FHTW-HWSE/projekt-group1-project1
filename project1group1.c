576f5c7e8ee6d8000ec79b63a5ebddb790a74821
Karin Lang <ic22b078@technikum-wien.at>
2023-05-20 15:34:37 +0200

    Add new function fully_occupied + Unit Test

--- project1group1.c
+++ project1group1.c
@@ -194,8 +194,17 @@ students[row * MAX_SEATS + seat].seat_number = seat;
 return row * MAX_SEATS + seat; // Return the seat index
 }*/
 
-
-
+/**
+ * @brief Generiert ein Belegungsmuster fuer eine volle Auslastung des Hoersaals
+ * @param room Ein 2D-Array mit dem Attribut available fuer die Verfuegbarkeit
+ * @param rows Die Anzahl an Sitzreihen des Saals
+ * @param seats Die Anzahl der Sitze pro Sitzreihe
+*/
+void fully_occupied(Seat room[][MAX_SEATS], int rows, int seats) {
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < seats; j++)
+            room[i][j].available = 1;
+}
 
 int main(void) {
     
@@ -256,6 +265,20 @@ int main(void) {
         printf("\nEtwas ist nicht nach Plan gelaufen.\n");
     }
 
+
+    /* Unit Test */
+
+    int rows = 5;
+    int seats = 3;
+    Seat room[MAX_ROWS][MAX_SEATS]={0};
+
+    fully_occupied(room, rows, seats);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < seats; j++)
+            printf("Sitzplatznummernausgabe: %d ", room[i][j]);
+
+
+
   /* Sitzplatzvergabe bei Eingabe einer Studierendenkennung */
     /*char student_id[MAX_TOKEN_LEN];
     printf("Geben Sie die Studierendenkennung ein: ");
@@ -278,118 +301,3 @@ int main(void) {
     return 0;
 }
 
-
-
-/*
-
-//************FIND AVAILABLEE SEAT AND ASSIGNE IT TO STUDEN**************
-
-//This function is checking int the room data what seats are still available, randomly selects one of the available seats
-//and assigns the selected seat to the user data
-
-void checkAvailableSeats(Room* room, User* users, int numUsers) {
-    int availableSeats[MAX_NUM_SEATS];
-    int numAvailableSeats = 0;
-
-    // Loop through each seat in the room
-    for (int i = 0; i < room->numSeats; i++) {
-        int seatNum = room->seats[i];
-
-        // Check if the seat is available
-        bool isAvailable = true;
-        for (int j = 0; j < numUsers; j++) {
-            if (users[j].seatNum == seatNum) {
-                isAvailable = false;
-                break;
-            }
-        }
-
-        // If the seat is available, add it to the availableSeats array
-        if (isAvailable) {
-            availableSeats[numAvailableSeats] = seatNum;
-            numAvailableSeats++;
-        }
-    }
-
-    // Randomly choose an available seat and assign it to the user
-    if (numAvailableSeats > 0) {
-        int randIndex = rand() % numAvailableSeats;
-        int chosenSeatNumber = availableSeats[randomIndex];
-         // loop to find the user with the corresponding studentID
-        for (int i = 0; i < NUM_USERS; i++) {
-            if (users[i].id == studentID) {
-                users[i].seatNumber = chosenSeatNumber; // the user will be assigned the chosen seat
-                printf("Seat %d has been assigned to student with ID %d\n", chosenSeatNumber, studentID);
-                break;
-        }
-    }
-}
-
-
-//**********************FUNCTION FOR REGISTRATION************************
-
-void registerForExam() {
-  // Print request to enter student ID to console
-  printf("Please enter your student ID to register for the exam:\n");
-  // Use scanf to read user input
-  int studentID;
-  scanf("%d", &studentID);
-
-  // Check if the entered ID is registered for the exam
-  bool isRegistered = checkRegistration(studentID);
-  if (!isRegistered) {
-    printf("Error: Student ID %d is not registered for the exam.\n", studentID);
-    return;
-  }
-
-  // Check which seats are still available in the room
-  readRooms();
-
-  // Randomly select an available seat
-  int seatNumber = selectSeat();
-
-  // Assign the selected seat to the user data
-  bool isAssigned = assignSeat(studentID, seatNumber);
-  if (!isAssigned) {
-    printf("Error: Student ID %d is already assigned to a seat.\n", studentID);
-    return;
-  }
-
-  printf("Seat assigned: Student ID %d is assigned to seat %d.\n", studentID, seatNumber);
-}
-
-
-//***********************************************************************
-//***********************************MAIN********************************
-//***********************************************************************
-
-int main() {
-    int choice;
-    readRooms();
-    readStudents();
-    
-    //Print main menue
-    while (1) {
-        printf("\n********** MENU **********\n");
-        printf("1. Register for Exam\n");
-        printf("2. Retrieve List of Neighbors\n");
-        printf("3. Quit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:    //If the user selects "Register for Exam" a function is called that requests to enter the student ID is printed to the console
-                registerForExam();
-                break;
-            case 2:
-                retrieveNeighbors();
-                break;
-            case 3:
-                printf("Quitting...\n");
-                return 0;
-            default:
-                printf("Invalid choice, please try again.\n");
-        }
-    }
-}
-        */
