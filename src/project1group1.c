4622b82411367f9031894f3e9246045117e4ce7c
Karin Lang <ic22b078@technikum-wien.at>
2023-06-01 20:42:21 +0200

    Add main program start

--- src/project1group1.c
+++ src/project1group1.c
@@ -5,9 +5,6 @@
 #include <math.h>
 #include "project1group1.h"
 
-
-
-
 /**
  * @brief Fordert den Benutzer auf eine Auswahl zu treffen, wie stark der Saal ausgelastet werden soll. Drei Moeglichkeiten stehen zur Auswahl.
  * @return Die Auswahl des Benutzers als Typ enum Selection
@@ -176,6 +173,26 @@ void fully_occupied(Seat room[][MAX_SEATS], int rows, int seats) {
 
 int main(void) {
     
+    Seat class[MAX_ROWS][MAX_SEATS]; 
+    char user_input[MAX_TOKEN_LEN];
+    int rows = 0;
+    int seats = 0;
+
+    printf("Servus! Dies ist eine Hilfestellung fuer die Belegung Ihres Klassenraumes.\n"
+        "Wenn es gerade nicht moeglich ist den Klassenraum vollstaendig zu fuellen,\n" 
+        "helfen wir Ihnen gerne eine passene Belegungsrate sowie einen angemessenen Sitzplan zu generieren.\n"
+        "Mithilfe Ihren Angaben wird ein passendes Model erstellt.\n"
+        "Bitte geben Sie an wieviele Sitzreihen es in Ihrem Hoersaal gibt:\n");
+    fgets(user_input,MAX_TOKEN_LEN,stdin);
+    rows = atoi(user_input);
+    printf("Bitte geben Sie an wieviele Sitze es pro Reihe gibt:\n\n");
+    fgets(user_input,MAX_TOKEN_LEN,stdin);
+    seats = atoi(user_input);// Schleife mit Error fehlt noch
+    
+    Selection selection = select_layout();
+
+
+
     return 0;
 }
 
